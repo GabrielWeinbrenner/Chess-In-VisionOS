0af603fa226499084e5c69dd8120e1985da76510
GabrielWeinbrenner <gabriel.weinbrenner@gmail.com>
2024-01-16 10:27:27 -0500

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,21 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Gabriel Weinbrenner on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }.immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
